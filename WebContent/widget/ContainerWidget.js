7ce8bfb1e3c3bfab7bf94df71f7bae0f872fbfe0
juea01 <akashwarkalay@gmail.com>
2017-07-04 15:42:48 +1200

    CSS style changes

--- WebContent/widget/ContainerWidget.js
+++ WebContent/widget/ContainerWidget.js
@@ -20,12 +20,7 @@ define([
 
 		//	define an onClick handler
 		_onClick: function(){
-			if(this._firstClicked){
-				this.titleNode.innerHTML = this.title + " was clicked " + (++this._counter) + " timeszzzzzz";
-			} else {
-				this.titleNode.innerHTML = this.title + " was clicked!";
-				this._firstClicked = true;
-			}
+			
 		},
 
 		postCreate: function(){
